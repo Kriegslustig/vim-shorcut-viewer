28746a6544255ec73a9034c6d23af904528836b5
Kriegslustig <github@ls7.ch>
2015-12-09 21:39:30 +0100

    Nicen up the output a bit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ manual.getMap('https://raw.githubusercontent.com/Kriegslustig/vim-index-json/mas
     var listener = keyInterpreter.listen()
     var modeListener = mode.render(Object.keys(man))
     window.addEventListener('vimkey', (e) => {
-      document.body.children[0].innerHTML = (manual.getMan(man, mode.get(), e.detail.key) || '')
+      document.body.children[0].innerHTML = (manual.getMan(man, mode.get(), e.detail.key) || '&nbsp;')
     })
   })
   .catch((err) => console.error(err))
