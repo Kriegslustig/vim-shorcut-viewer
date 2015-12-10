ea983d35489659b7d61b3f12a66bf82f2c3c8f2c
Kriegslustig <github@ls7.ch>
2015-12-10 22:34:19 +0100

    Add some nice stuff to the GUI

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ manual.getMap('https://raw.githubusercontent.com/Kriegslustig/vim-index-json/mas
     var modeListener = mode.render(Object.keys(man))
     window.addEventListener('vimkey', (e) => {
       document.body.children[0].innerHTML = (manual.getMan(man, mode.get(), e.detail.key) || '&nbsp;')
+      document.body.children[1].innerHTML = (e.detail.key || '&nbsp;')
     })
   })
   .catch((err) => console.error(err))
