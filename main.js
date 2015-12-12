19ca856b62d224fb29006698b187745d5fbbda2e
Kriegslustig <github@ls7.ch>
2015-12-12 18:18:32 +0100

    Add a graphical keyboard

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 manual.getMap('https://raw.githubusercontent.com/Kriegslustig/vim-index-json/master/index.json')
   .then((man) => {
+    window.man = man
     var listener = keyInterpreter.listen()
     var modeListener = mode.render(Object.keys(man))
     window.addEventListener('vimkey', (e) => {
-      document.body.children[0].innerHTML = (manual.getMan(man, mode.get(), e.detail.key) || '&nbsp;')
-      document.body.children[1].innerHTML = (e.detail.key || '&nbsp;')
+      if(!e.detail.key) return
+      document.body.children[0].textContent = (manual.getMan(man, mode.get(), e.detail.key) || '_')
+      document.body.children[1].textContent = (e.detail.key)
     })
   })
   .catch((err) => console.error(err))
