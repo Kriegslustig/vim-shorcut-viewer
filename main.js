47217a2d57ec13535fe87b9f5893a074d8d91374
Kriegslustig <github@ls7.ch>
2015-12-14 10:08:49 +0100

    Implement a simpler keyView

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 manual.getMap('https://raw.githubusercontent.com/Kriegslustig/vim-index-json/master/index.json')
   .then((man) => {
     window.man = man
-    var listener = keyInterpreter.listen()
-    var modeListener = mode.render(Object.keys(man))
+    var keyContainer = document.querySelector('.key-combination')
+    var resultContainer = document.querySelector('.result > p')
+    var modeListeners = mode.render(Object.keys(man))
+    var keyListener = keyInterpreter.listen()
     window.addEventListener('vimkey', (e) => {
       if(!e.detail.key) return
-      document.body.children[0].textContent = (manual.getMan(man, mode.get(), e.detail.key) || '_')
-      document.body.children[1].textContent = (e.detail.key)
+      keyView.updateKeys(keyContainer, e.detail.key)
+      resultContainer.textContent = (manual.getMan(man, mode.get(), e.detail.key.join('')) || '_')
     })
   })
   .catch((err) => console.error(err))
